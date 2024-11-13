fe1dac3e3dbc9611a8558984267419dafc69bffe
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-13 14:53:27 +0530

    strict-mode

---
+++ strictmode.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let hasDriversLicence = false;
+
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicence = true;
+}
+
+if (hasDriversLicence) {
+  console.log(`you passed the test`);
+}
+//let if = "yashwanth"
+
+const interface = "yashwanth";
