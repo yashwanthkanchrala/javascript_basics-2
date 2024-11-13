6bfee03db19e6741737a0099f1dc63b5fb4b89c2
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-13 16:07:01 +0530

    functions-2

---
+++ functions-2.js
@@ -0,0 +1,16 @@
+"use strict";
+//function declaration
+function calcage1(birthyear) {
+  return 2047 - birthyear;
+}
+
+const age1 = calcage1(1996);
+console.log(age1);
+
+//function expression
+
+const calcage2 = function (birthyear) {
+  return 2046 - birthyear;
+};
+const age2 = calcage2(1996);
+console.log(age2);
