0e9f01477732b84bfad888d8e013db5e348aba45
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-13 22:45:51 +0530

    arrays

---
+++ basic-arrays-operation.js
@@ -0,0 +1,25 @@
+"use-script";
+const friend = ["yashwanth", "lohith", "vijay", "sai"];
+//add elements
+friend.push("thanmai");
+console.log(friend);
+console.log(friend.length);
+
+//const newLength = friend.push("thanmai");
+//console.log(newLength);
+friend.unshift("vinesh");
+console.log(friend);
+
+//remove elements
+const pop = friend.pop(); //removes lastv element
+console.log(friend);
+console.log(pop);
+
+const shift = friend.shift();
+console.log(shift);
+console.log(friend);
+
+console.log(friend.indexOf("sai"));
+console.log(friend.indexOf("jjnk"));
+console.log(friend.includes("sai"));
+console.log(friend.includes("jjnk"));
