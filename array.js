d55f45db17299ac370b29aeff211609493be57fb
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-13 21:21:43 +0530

    arrays

---
+++ array.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const friends = ["yashwanth", "sai", "vinesh", "vinod"];
+console.log(friends);
+
+const years = new Array(1996, 1997, 1998, 1999);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+console.log(friends.length);
+console.log(friends[friends.length - 2]);
+
+friends[2] = "thanmai";
+console.log(friends);
+
+const firstName = "kancharla";
+
+const array = [firstName, 20 + 16, firstName + " " + "yashwanth", friends];
+
+console.log(array);
+
+console.log(array.length);
